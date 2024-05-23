209483ef616cca3dd145b809d2630e5bc82dcd6e
glenn <glenn.luna@edu.sait.ca>
2024-05-23 15:23:08 -0600

    this is my assignment

---
+++ app/week-2/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <main>
+        <h1>Shopping List</h1>
+      </main>
+    );
+  }
+  
---
+++ app/week-2/studentinfo.js
@@ -0,0 +1,9 @@
+export default function Studentinfo() {
+    return (
+      <main>
+        <h1>name: Glenn Luna</h1>
+        <h2></h2>
+      </main>
+    );
+  }
+  
