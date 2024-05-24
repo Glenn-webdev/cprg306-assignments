8de2c557d837c71fd1fae5766c437f70d4f73a5d
glenn <glenn.luna@edu.sait.ca>
2024-05-23 21:18:34 -0600

    remove github link

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -7,8 +7,10 @@ const StudentInfo = () => {
             <h1>Name: Glenn Luna</h1>
             
             
-            <Link href="https://cprg306-assignments-lilac-three.vercel.app/" class="text-blue-600 visited:text-purple-600 ...">
-                <h2 class="underline underline-offset-2 ...">Git Hub</h2></Link>
+            
+
+                <Link href="https://cprg306-assignments-lilac-three.vercel.app/" class="text-blue-600 visited:text-purple-600 ...">
+                <h2 class="underline underline-offset-2 ...">GitHub</h2></Link>
               
         </div>
         
