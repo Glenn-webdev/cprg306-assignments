9e2a2fa7ffacb50b42b0d9a42f08c3dfd4de8682
glenn <glenn.luna@edu.sait.ca>
2024-05-23 21:02:08 -0600

    third commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
 
-
+import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to WebDev</h1>
+          <div justify-center>
+                     
+              <h1>CPRG 306: Web Development 2 - Assignments</h1>
+          
+          
+          <Link href ="/week-2" class="text-blue-600 visited:text-purple-600 ..."><h2 class="underline underline-offset-2 ...">WEEK 2</h2></Link>
+          </div>
     </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,18 @@
+import react from "react";
+import Link from 'next/link';
+
+const StudentInfo = () => {
+    return (
+        <div>
+            <h1>Name: Glenn Luna</h1>
+            <h2>Course: Software Development</h2>
+            
+            <Link href="https://cprg306-assignments-lilac-three.vercel.app/" class="text-blue-600 visited:text-purple-600 ...">
+                <h2 class="underline underline-offset-2 ...">Git Hub</h2></Link>
+              
+        </div>
+        
+    );
+};
+
+export default StudentInfo
