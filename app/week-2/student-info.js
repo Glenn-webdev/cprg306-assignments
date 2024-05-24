c44184d41e3923e5e1db38bde228180e856f191b
glenn <glenn.luna@edu.sait.ca>
2024-05-23 21:13:41 -0600

    4th commit

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -5,7 +5,7 @@ const StudentInfo = () => {
     return (
         <div>
             <h1>Name: Glenn Luna</h1>
-            <h2>Course: Software Development</h2>
+            
             
             <Link href="https://cprg306-assignments-lilac-three.vercel.app/" class="text-blue-600 visited:text-purple-600 ...">
                 <h2 class="underline underline-offset-2 ...">Git Hub</h2></Link>
