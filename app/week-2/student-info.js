03eb792e700b2956d01414af2b54dd5849b17977
glenn <glenn.luna@edu.sait.ca>
2024-05-25 15:03:55 -0600

    replace const with edf

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,14 +1,14 @@
 import react from "react";
 import Link from 'next/link';
 
-const StudentInfo = () => {
+export default function StudentInfo () 
+    
+
+{
     return (
         <div>
             <h1>Name: Glenn Luna</h1>
-            
-            
-            
-
+                  
                 <Link href="https://cprg306-assignments-lilac-three.vercel.app/" class="text-blue-600 visited:text-purple-600 ...">
                 <h2 class="underline underline-offset-2 ...">GitHub</h2></Link>
               
@@ -17,4 +17,3 @@ const StudentInfo = () => {
     );
 };
 
-export default StudentInfo
