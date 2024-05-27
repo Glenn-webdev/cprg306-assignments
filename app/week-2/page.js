d82bfd728b8a07810b75d19798b160e717f02651
glenn <glenn.luna@edu.sait.ca>
2024-05-27 10:11:27 -0600

    update

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -3,7 +3,7 @@ import StudentInfo from "./student-info";
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div class justify><h1>CPRG 306: Web Development 2 - Assignments</h1>
+        <div class justify><h1>Shopping List</h1>
         <StudentInfo></StudentInfo>
         </div>
       </main>
