8b0e808283dfae5ba6abd82d9723f02bb8c72102
glenn <glenn.luna@edu.sait.ca>
2024-05-27 09:44:16 -0600

    update

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -9,7 +9,7 @@ export default function StudentInfo ()
         <div>
             <h1>Name: Glenn Luna</h1>
                   
-                <Link href="https://cprg306-assignments-lilac-three.vercel.app/" class="text-blue-600 visited:text-purple-600 ...">
+                <Link href="https://github.com/Glenn-webdev/cprg306-assignments" class="text-blue-600 visited:text-purple-600 ...">
                 <h2 class="underline underline-offset-2 ...">GitHub</h2></Link>
               
         </div>
