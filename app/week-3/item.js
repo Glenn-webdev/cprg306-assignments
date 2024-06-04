3e61d3eb35ea0633408d10cf23d78532a213a96f
glenn <glenn.luna@edu.sait.ca>
2024-06-03 22:56:22 -0600

    change bg-gradiennt

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -2,7 +2,7 @@ const Item = ({name,quantity,category}) => {
 
     return (
         <li>
-            <div className=' w-60 h-35 py-8  text-center border-2 rounded-full shadow-md'>
+            <div className=' w-60 h-35 py-8  text-center border-2 rounded-lg shadow-md  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
                 <p className="text-sm font-semibold">Name: {name}</p>
                 <p className="text-sm font-semibold">Category: {category}</p>
             
