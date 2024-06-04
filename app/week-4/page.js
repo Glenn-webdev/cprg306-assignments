345d7fab32cf5f69bf324d68b08391f3f1d301cb
glenn <glenn.luna@edu.sait.ca>
2024-06-03 23:33:15 -0600

    edit bg collor week 4

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,3 +1,15 @@
 
+export default function page() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-red-400">
+        <div>
+            <h1 className="font-extrabold text-center ">Under maintenance</h1>
+        </div>
+      </main>
 
+
+
+    )
+ 
+};
 
