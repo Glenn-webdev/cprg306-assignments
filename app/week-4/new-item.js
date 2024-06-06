cfd291fd7805fbb04c090574087f0474f2085ed7
Glenn Luna <140766061+Glenn-webdev@users.noreply.github.com>
2024-06-05 22:43:51 -0600

    space added in alert

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -24,7 +24,7 @@ export default function NewItem() {
        
 
         console.log(item);
-        alert(`Name: ${name}Quantity: ${quantity} Category: ${category}`);
+        alert(`Name: ${name} Quantity: ${quantity} Category: ${category}`);
 
 
     };
