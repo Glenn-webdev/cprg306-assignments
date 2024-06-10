5572674444ced64edd9d38406dc3553344aa0226
glenn <glenn.luna@edu.sait.ca>
2024-06-10 09:32:59 -0600

    update function item

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -7,28 +7,48 @@ export default function NewItem() {
     const [name,setName] = useState("");
     const [quantity,setQuantity] = useState(1);
     const [category,setCategory] = useState("Produce");
+      
 
-    function logValue() {
-        console.log(category);
-    }
+    const handleSubmit = (event) => {
+         event.preventDefault(); 
+    const item = {name,category,quantity};
+         
+    if (name == "") {
+        alert("Name is empty!");  
+     
 
-        
+         } 
 
-    const handleSubmit = (e) => { e.preventDefault(); 
-
-        const item = {name,category,quantity};
-
-    
-
-    
-       
-
-        console.log(item);
-        alert(`Name: ${name} Quantity: ${quantity} Category: ${category}`);
-
-
+         if (quantity > 10) 
+            {
+                alert("Quantity is more than 10!");  
+            }
+         
+         
+         else {
+            console.log(item);
+            alert(`Name: ${name} Quantity: ${quantity} Category: ${category}`);
+            
+        }    
     };
 
+    
+
+
+    const handleTextChange = (event) => {
+        setName(event.target.value);
+
+    }
+   
+    const handleQuantityChange = (event) => {
+          setQuantity(event.target.value);
+        
+      
+
+    }
+    const handleCategoryChange = (event) => {
+        setCategory(event.target.value);
+    }
 
     return (
 
@@ -38,21 +58,21 @@ export default function NewItem() {
                     <label for="name">Name</label>
                     </div>
                     <div> 
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+                    <input type="text" value={name} onChange={handleTextChange}/>
                 </div>
                
                 <div>
                     <label for="quantity">Quantity</label>
                     </div>
                     <div>
-                    <input type="number" value={quantity} onChange={(e) => setQuantity(e.target.value)}/>
+                    <input type="number" value={quantity} onChange={handleQuantityChange}/>
                 </div>
                 <div>
                 <label for="category">Category</label>
                 </div>
                     <div>
 
-                        <select value={category} onChange={(e) => setCategory(e.target.value)} >
+                        <select value={category} onChange={handleCategoryChange} >
                             <option value="Produce">Produce</option>
                             <option value="Baker">Baker</option>
                             <option value="Meat">Meat</option>
@@ -69,7 +89,7 @@ export default function NewItem() {
                     
                     </div>
                     <div class ='py-4 flex justify-center '>
-                        <button onClick={logValue} className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Submit</button>
+                        <button onClick={console.log(category)} className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Submit</button>
                     </div>
                              
                  </div>
