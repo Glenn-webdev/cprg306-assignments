93b6bf01640a78b2b8c8164eddab93447a53b5ad
glenn <glenn.luna@edu.sait.ca>
2024-06-24 05:42:39 -0600

    copy files week5 and copy file new-item.js

---
+++ app/week-6/item-list.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import itemsData from '../week-5/item.json'; 
+import Item from './item';
+
+
+const ItemList = () => {
+  const [items, setItems] = useState([]);
+  const [sortBy, setSortBy] = useState('name'); 
+
+  useEffect(() => {
+   
+   
+    setTimeout(() => {
+    
+      
+      const sortedItems = [...itemsData].sort((a, b) => {
+        if (sortBy === 'name') {
+          return a.name.localeCompare(b.name);
+        } else if (sortBy === 'category') {
+          return a.category.localeCompare(b.category);
+        } else {
+          return 0;
+        }
+      });
+      setItems(sortedItems);}, 1000);}, [sortBy]); 
+
+  return (
+    <div>
+      <h2 class='font-extrabold text-center bg-clip-padding'>Shopping List</h2>
+      <div>
+
+
+         <label id="sort">Sort by:</label>
+
+        
+         <div className='space-x-5 px-5 shadow-lg'>
+         <button className="rounded-lg px-4 border-neutral-600"
+            data-value="name"
+            onClick={(e) => setSortBy(e.target.getAttribute('data-value'))}
+            style={{ backgroundColor: sortBy === 'name' ? 'blue' : 'grey', color: 'white' }}
+          >
+            Name
+          </button>
+
+          <button className="rounded-lg px-4"
+            data-value="category"
+            onClick={(e) => setSortBy(e.target.getAttribute('data-value'))}
+            style={{ backgroundColor: sortBy === 'category' ? 'blue' : 'grey', color: 'white' }}
+          >
+            Category
+          </button>
+    </div>
+
+     
+      
+      </div>
+
+
+      <div className='pl-10 pt-4 pb-4'>
+        {items.map(item => (
+          
+          <Item key={item.id}
+          name={item.name}
+          category={item.category}
+          quantity={item.quantity}></Item>
+        ))} 
+      </div>
+    </div>
+  );
+};
+
+export default ItemList;
---
+++ app/week-6/new-item.js
@@ -0,0 +1,113 @@
+"use client";
+
+import { useState } from "react";
+
+export default function NewItem() {
+
+    const [name,setName] = useState("");
+    const [quantity,setQuantity] = useState(1);
+    const [category,setCategory] = useState("Produce");
+      
+
+    const handleSubmit = (event) => {
+         event.preventDefault(); 
+    const item = {name,category,quantity};
+
+    
+       
+    if (name == "") {
+        alert("Name is empty!");  
+         return;  
+         
+        } 
+
+     if (quantity > 10) {
+        alert("quantity is more thant 10!")
+        return;
+     }   
+
+            console.log(item);
+            alert(`Name: ${name} Quantity: ${quantity} Category: ${category}`);
+            
+    
+    }; 
+
+    
+
+
+    const handleTextChange = (event) => {
+        setName(event.target.value);
+
+    }
+   
+    const handleQuantityChange = (event) => {
+          setQuantity(event.target.value);
+        
+      
+
+    }
+    const handleCategoryChange = (event) => {
+        setCategory(event.target.value);
+    }
+
+    return (
+
+        <form onSubmit={handleSubmit}>
+            <div class="border border-indigo-600 flex-initial p-4 rounded-lg bg-red bg-zinc-300">
+                <div>
+                    <label for="name">Name</label>
+                    </div>
+                    <div> 
+                    <input type="text" value={name} onChange={handleTextChange}/>
+                </div>
+               
+                <div>
+                    <label for="quantity">Quantity</label>
+                    </div>
+                    <div>
+                    <input type="number" value={quantity} onChange={handleQuantityChange}/>
+                </div>
+                <div>
+                <label for="category">Category</label>
+                </div>
+                    <div>
+
+                        <select value={category} onChange={handleCategoryChange} >
+                            <option value="Produce">Produce</option>
+                            <option value="Baker">Baker</option>
+                            <option value="Meat">Meat</option>
+                            <option value="Frozen Foods">Frozen Foods</option>
+                            <option value="Canned Goods">Canned Goods</option>
+                            <option value="Dry Goods">Dry Goods</option>
+                            <option value="Beverage">Beverage</option>
+                            <option value="Snacks">Snacks</option>
+                            <option value="Household">Household</option>
+                            <option value="Others">Others</option>
+
+
+                        </select>     
+                    
+                    </div>
+                    <div class ='py-4 flex justify-center '>
+                        <button onClick={console.log(category)} className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Submit</button>
+                    </div>
+                             
+                 </div>
+
+</form>
+
+
+
+
+
+
+    
+        
+           
+        
+       
+        
+
+    )
+    
+};
