3c60e4ff112f83e51ed1aeb983e0d8f58542c2a0
glenn <glenn.luna@edu.sait.ca>
2024-06-24 06:03:45 -0600

    first commit for page.js

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,15 +1,29 @@
 
 "use client";
-import React from "react";
+
+import React,{useState} from "react";
 import ItemList from "../week-5/item-list";
+import NewItem from "./new-item";
+import itemsData from "../week-5/item.json";
 
 export default function page() {
+    
+        const [items, setItems] = useState([...itemsData]); // Initialize items with data from items.json
+      
+        // Event handler to add a new item
+        const handleAddItem = (newItem) => {
+          setItems((prevItems) => [...prevItems, newItem]);
+        };
+    
+
     return (
         <div>
-            <ItemList></ItemList>
+           
+            <NewItem onAddItem = {handleAddItem}></NewItem>
+            <ItemList items={items}></ItemList>
+
         </div>
-    )
-   
+    ) 
 
 
-};
+}
